dfca3e2d2945fbf2052d20a038bb80531fadbb29
mvn-vangpham-hn <vang.pham@monstar-lab.com>
2023-06-21 11:10:42 +0700

    faet: add lib aos

--- src/js/home.js
+++ src/js/home.js
@@ -1,1 +1,9 @@
 import '../styles/pages/home.css'
+import AOS from 'aos'
+import 'aos/dist/aos.css' // You can also use <link> for styles
+
+AOS.init({
+  duration: 600,
+  easing: 'ease-in-sine',
+  delay: 100,
+})
