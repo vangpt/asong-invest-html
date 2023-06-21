57938e5d2bd5756f289b68a1db1d05946bf03b77
mvn-vangpham-hn <vang.pham@monstar-lab.com>
2023-06-21 20:24:39 +0700

    feat: update aos 2

--- src/js/home.js
+++ src/js/home.js
@@ -2,8 +2,11 @@ import '../styles/pages/home.css'
 import AOS from 'aos'
 import 'aos/dist/aos.css' // You can also use <link> for styles
 
-AOS.init({
-  duration: 600,
-  easing: 'ease-in-sine',
-  delay: 100,
+window.addEventListener('DOMContentLoaded', (e) => {
+  AOS.init({
+    duration: 600,
+    easing: 'ease-in-sine',
+    delay: 100,
+    once: true,
+  })
 })
--- src/js/news-detail.js
+++ src/js/news-detail.js
@@ -1,10 +1,1 @@
 import '../styles/pages/news-detail.css'
-import AOS from 'aos'
-import 'aos/dist/aos.css' // You can also use <link> for styles
-
-AOS.init({
-  offset: 200,
-  duration: 600,
-  easing: 'ease-in-sine',
-  delay: 300,
-})
